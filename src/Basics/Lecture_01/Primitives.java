0019362884f65a1ced6b5d2c74086d8478852ec7
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-07-25 14:37:20 +0530

    lecture 1 part 2

---
+++ src/Basics/Lecture_01/Primitives.java
@@ -0,0 +1,23 @@
+package Basics.Lecture_01;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        //primitive - any data type we can't break even further
+        int rollNo = 64; //can store 4 bytes
+        char letter = 'a';
+        float marks = 98.6f; //can store 4 bytes
+        double largeDecimalNumbers = 723474.3434; //can store 8 bytes
+        long largeInteger = 5485487598475L; //can store 8 bytes
+        boolean check = true; //or false
+
+        /*
+        all decimal values are of type 'double'
+        so we have to add 'f' at end to store it in float
+         */
+
+        //String is not primitive
+        //we can break String into char
+        String name = "John";
+    }
+}
