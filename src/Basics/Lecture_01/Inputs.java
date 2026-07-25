64eaaa6203c1574d5bbc3626ee5ea5831c2bdb32
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-07-25 14:47:29 +0530

    lecture 1 part 3

---
+++ src/Basics/Lecture_01/Inputs.java
@@ -0,0 +1,25 @@
+package Basics.Lecture_01;
+
+import java.util.Scanner;
+
+public class Inputs {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Enter your roll no: ");
+//        int rollno = input.nextInt();
+//        System.out.println("your roll no is: "+rollno);
+
+
+//        System.out.println("Enter your name: ");
+//        String name = input.next();
+//        System.out.println("your name is "+name);
+
+
+//        System.out.println("enter your marks");
+//        float marks = input.nextFloat();
+//        System.out.println("your marks are "+marks);
+    }
+
+}
